ede684a5376f3735c381f680e3a8bba4d8303cbd
BertinManoelBr <manoelcarlobertin@outlook.com>
2025-07-07 23:51:15 -0300

    o recurso category foi espelhado no banco de dados.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_08_024218) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_08_024733) do
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
