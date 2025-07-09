5b62e95896c2fb3013faf08fd9d3319c8aad8eae
BertinManoelBr <manoelcarlobertin@outlook.com>
2025-07-09 20:21:23 -0300

    ajustei a factory usando chatGPT

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
+  # associations
   belongs_to :productable, polymorphic: true
 
   # validations
--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :product do
-    name { "MyString" }
-    description { "MyText" }
-    price { 1 }
-    productable { nil }
+    sequence(:name) { |n| "Produto #{n}" }
+    description { Faker::Lorem.sentence(word_count: 10) }
+    price { Faker::Number.between(from: 10, to: 1000) }
+    association :productable, factory: :category, strategy: :build
   end
 end
