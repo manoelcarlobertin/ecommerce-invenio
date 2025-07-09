3b783103df33baa07a7d7d75250329e44fddcb16
BertinManoelBr <manoelcarlobertin@outlook.com>
2025-07-09 19:57:35 -0300

    criei algumas validações no model Product e passaram 2 testes. Faltam 3 testes.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,8 @@
 class Product < ApplicationRecord
   belongs_to :productable, polymorphic: true
+
+  # validations
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
+  validates :description, presence: true
+  validates :price, numericality: { greater_than: 0 }
 end
